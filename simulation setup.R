8c27abd903134e168fea8b2ec4737c59f0807f57
Kunicki <zkunicki@ad.brown.edu>
2019-12-02 12:25:40 -0500

    code algebra for GRM

--- simulation setup.R
+++ simulation setup.R
@@ -129,6 +129,91 @@ n500r6 <- n500r6 + ggtitle("Test Information Curves", subtitle = "N = 500, r = 0
   scale_fill_grey()
 
 
+## Testing to extract item info
+r = .8
+n = 1000
+
+R = matrix(cbind(1, r, r, r, r, r, r, r, r, r,  #design matrix
+                 r, 1, r, r, r, r, r, r, r, r,  
+                 r, r, 1, r, r, r, r, r, r, r,
+                 r, r, r, 1, r, r, r, r, r, r,
+                 r, r, r, r, 1, r, r, r, r, r,
+                 r, r, r, r, r, 1, r, r, r, r,
+                 r, r, r, r, r, r, 1, r, r, r,
+                 r, r, r, r, r, r, r, 1, r, r,
+                 r, r, r, r, r, r, r, r, 1, r,
+                 r, r, r, r, r, r, r, r, r, 1), 
+           nrow=10)
+U = t(chol(R)) #transposing design matrix, cholesky decomposition
+nvars = dim(U)[1] #selecting number of variables
+n = n #set number of observations
+random.normal = matrix(rnorm(nvars*n, 0, 1), nrow=nvars, ncol=numobs);
+X = U %*% random.normal
+newX = t(X)
+
+raw = as.data.frame(newX)
+prob_3cat   <- c(.25, .50, .25)
+prob_5cat   <- c(.1, .2, .4, .2, .1)
+prob_7cat   <- c(.06, .12, .19, .26, .19, .12, .06)
+prob_9cat   <- c(.04, .08, .12, .16, .20, .16, .12, .08, .04)
+prob_11cat  <- c(.03, .06, .08, .11, .14, .16, .14, .11, .08, .06, .03)
+#putting data into likert-form
+orig.likert.3cat <- raw %>% mutate(V1 = norm2likert(V1, prob = prob_3cat),
+                                   V2 = norm2likert(V2, prob = prob_3cat),
+                                   V3 = norm2likert(V3, prob = prob_3cat),
+                                   V4 = norm2likert(V4, prob = prob_3cat),
+                                   V5 = norm2likert(V5, prob = prob_3cat),
+                                   V6 = norm2likert(V6, prob = prob_3cat),
+                                   V7 = norm2likert(V7, prob = prob_3cat),
+                                   V8 = norm2likert(V8, prob = prob_3cat),
+                                   V9 = norm2likert(V9, prob = prob_3cat),
+                                   V10 = norm2likert(V10, prob = prob_3cat))
+
+orig.likert.5cat <- raw %>% mutate(V1 = norm2likert(V1, prob = prob_5cat),
+                                   V2 = norm2likert(V2, prob = prob_5cat),
+                                   V3 = norm2likert(V3, prob = prob_5cat),
+                                   V4 = norm2likert(V4, prob = prob_5cat),
+                                   V5 = norm2likert(V5, prob = prob_5cat),
+                                   V6 = norm2likert(V6, prob = prob_5cat),
+                                   V7 = norm2likert(V7, prob = prob_5cat),
+                                   V8 = norm2likert(V8, prob = prob_5cat),
+                                   V9 = norm2likert(V9, prob = prob_5cat),
+                                   V10 = norm2likert(V10, prob = prob_5cat))
+
+orig.likert.7cat <- raw %>% mutate(V1 = norm2likert(V1, prob = prob_7cat),
+                                   V2 = norm2likert(V2, prob = prob_7cat),
+                                   V3 = norm2likert(V3, prob = prob_7cat),
+                                   V4 = norm2likert(V4, prob = prob_7cat),
+                                   V5 = norm2likert(V5, prob = prob_7cat),
+                                   V6 = norm2likert(V6, prob = prob_7cat),
+                                   V7 = norm2likert(V7, prob = prob_7cat),
+                                   V8 = norm2likert(V8, prob = prob_7cat),
+                                   V9 = norm2likert(V9, prob = prob_7cat),
+                                   V10 = norm2likert(V10, prob = prob_7cat))
+
+orig.likert.9cat <- raw %>% mutate(V1 = norm2likert(V1, prob = prob_9cat),
+                                   V2 = norm2likert(V2, prob = prob_9cat),
+                                   V3 = norm2likert(V3, prob = prob_9cat),
+                                   V4 = norm2likert(V4, prob = prob_9cat),
+                                   V5 = norm2likert(V5, prob = prob_9cat),
+                                   V6 = norm2likert(V6, prob = prob_9cat),
+                                   V7 = norm2likert(V7, prob = prob_9cat),
+                                   V8 = norm2likert(V8, prob = prob_9cat),
+                                   V9 = norm2likert(V9, prob = prob_9cat),
+                                   V10 = norm2likert(V10, prob = prob_9cat))
+
+orig.likert.11cat <- raw %>% mutate(V1 = norm2likert(V1, prob = prob_11cat),
+                                    V2 = norm2likert(V2, prob = prob_11cat),
+                                    V3 = norm2likert(V3, prob = prob_11cat),
+                                    V4 = norm2likert(V4, prob = prob_11cat),
+                                    V5 = norm2likert(V5, prob = prob_11cat),
+                                    V6 = norm2likert(V6, prob = prob_11cat),
+                                    V7 = norm2likert(V7, prob = prob_11cat),
+                                    V8 = norm2likert(V8, prob = prob_11cat),
+                                    V9 = norm2likert(V9, prob = prob_11cat),
+                                    V10 = norm2likert(V10, prob = prob_11cat))
+
+
 
 ## Extracting item informations
 
@@ -185,3 +270,22 @@ plot(eleven_cat_graded, type = 'info')
 
 #plotting extracted test info 
 plot(y = test.info.11.data$mirt, x = test.info.11.data$V11)
+
+
+plot(Theta, info.11.10, type = 'l', main = 'Item information')
+
+ICCplot = ggplot()+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.1), color = "blue")+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.2), color = "black")+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.3), color = "red")+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.4), color = "violet")+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.5), color = "orange")+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.6), color = "brown")+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.7), color = "gray")+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.8), color = "darkorchid")+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.9), color = "orchid")+
+  geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.10), color = "green")+
+  xlab('Theta') + ylab('Item Information')
+
+print(ICCplot)
+
