2062f064c1dc37f98ee19fda6867a42254096627
Kunicki <zkunicki@ad.brown.edu>
2019-12-05 13:20:14 -0500

    like 6 hours worth of poorly annotated work

--- simulation setup.R
+++ simulation setup.R
@@ -102,13 +102,14 @@ likert_test <- function(n, r){
   
   #extracing test information curves
   
-  Theta <- matrix(seq(-6, 6, .01))
+  Theta <- matrix(seq(-6, 6, .0001))
   test_info_data <- data.frame(Three = testinfo(three_cat_graded, Theta),
                                Five  = testinfo(five_cat_graded, Theta),
                                Seven = testinfo(seven_cat_graded, Theta),
                                Nine  = testinfo(nine_cat_graded, Theta),
                                Eleven = testinfo(eleven_cat_graded, Theta),
-                               theta = Theta)
+                               theta = Theta
+)
   test_info_data <- test_info_data %>% mutate(`Likert Category` = row_number())
   
   #putting data into long form for plot
@@ -123,10 +124,66 @@ likert_test <- function(n, r){
 
 n500r6 <- likert_test(500, .6)
 
-n500r6 <- n500r6 + ggtitle("Test Information Curves", subtitle = "N = 500, r = 0.60") + 
-  ylab("Test Information") + 
-  xlab("Theta") +
-  scale_fill_grey()
+n5000r8 <- likert_test(5000, .8)
+
+testdata <- n5000r8$data
+
+rxx9 = 10
+rxx95 = 20
+
+ggplot(testdata, aes(x = theta, y = testinfo, color = `Likert Category`)) + 
+  geom_line(show.legend = FALSE) +
+  scale_color_manual(values = c("#fdd49e", "#fdbb84", "#fc8d59", "#ef6548", "#d7301f"))+
+  labs(title = "Test Information Plot", subtitle = "N = 5000, r = 0.80", y = "Test Information",
+       caption = "When Test Information = 10, Reliability = 0.90, When Test Information = 20, Reliability = 0.95") +
+  xlab(expression(theta)) +
+  geom_hline(yintercept = rxx9)+ 
+  geom_hline(yintercept = rxx95)
+
+testdata2 <- n500r6$data
+
+ggplot(testdata2, aes(x = theta, y = testinfo, color = `Likert Category`)) + 
+  geom_line(show.legend = FALSE) +
+  scale_color_manual(values = c("#fdd49e", "#fdbb84", "#fc8d59", "#ef6548", "#d7301f"))+
+  labs(title = "Test Information Plot", subtitle = "N = 500, r = 0.60", y = "Test Information",
+       caption = "When Test Information = 10, Reliability = 0.90, When Test Information = 20, Reliability = 0.95") +
+  xlab(expression(theta)) +
+  geom_hline(yintercept = rxx9)+ 
+  geom_hline(yintercept = rxx95)
+
+testdata3 <- n5000r7$data
+ggplot(testdata3, aes(x = theta, y = testinfo, color = `Likert Category`)) + 
+  geom_line() +
+  scale_color_manual(values = c("firebrick", "sandybrown", "darkorchid", "red4", "slategrey"))+
+  labs(title = "Test Information Plot", subtitle = "N = 5000, r = 0.70", y = "Test Information",
+       caption = "When Test Information = 10, Reliability = 0.90, When Test Information = 20, Reliability = 0.95") +
+  xlab(expression(theta)) +
+  geom_hline(yintercept = rxx9)+ 
+  geom_hline(yintercept = rxx95)
+
+
+
+testdata_three <- filter(testdata, `Likert Category` == "Three")
+testdata_five <- filter(testdata, `Likert Category` == "Five")
+testdata_seven <- filter(testdata, `Likert Category` == "Seven")
+testdata_nine <- filter(testdata, `Likert Category` == "Nine")
+testdata_eleven <- filter(testdata, `Likert Category` == "Eleven")
+
+TestInfoPlot = 
+ggplot()+
+  geom_line(data = testdata_three, aes(x = Theta, y = testinfo, color = "firebrick"))+
+  geom_line(data = testdata_five, aes(x = Theta, y = testinfo, color = "sandybrown"))+
+  geom_line(data = testdata_seven, aes(x = Theta, y = testinfo, color = "darkorchid"))+
+  geom_line(data = testdata_nine, aes(x = Theta, y = testinfo, color = "red4"))+
+  geom_line(data = testdata_eleven, aes(x = Theta, y = testinfo, color = "slategrey")) +
+  labs(title = "Test Information Plot", subtitle = "N = 5000, r = 0.80", y = "Test Information") +
+  xlab(expression(theta))+
+  scale_color_discrete(name = "Number of Likert Categories", labels = c("Three", "Five", "Seven", "Nine", "Eleven"))
+print(TestInfoPlot)
+
+
+
+
 
 
 ## Testing to extract item info
@@ -216,8 +273,42 @@ orig.likert.11cat <- raw %>% mutate(V1 = norm2likert(V1, prob = prob_11cat),
 
 
 ## Extracting item informations
-
+three_cat_graded <- mirt(orig.likert.3cat, 1, itemtype = 'graded')
+five_cat_graded <- mirt(orig.likert.5cat, 1, itemtype = 'graded')
+seven_cat_graded <- mirt(orig.likert.7cat, 1, itemtype = 'graded')
+nine_cat_graded <- mirt(orig.likert.9cat, 1, itemtype = 'graded')
 eleven_cat_graded <- mirt(orig.likert.11cat, 1, itemtype = 'graded')
+
+Theta <- matrix(seq(-6, 6, .001))
+test_info_data <- data.frame(Three = testinfo(three_cat_graded, Theta),
+                             Five  = testinfo(five_cat_graded, Theta),
+                             Seven = testinfo(seven_cat_graded, Theta),
+                             Nine  = testinfo(nine_cat_graded, Theta),
+                             Eleven = testinfo(eleven_cat_graded, Theta),
+                             theta = Theta
+)
+test_info_data <- test_info_data %>% mutate(`Likert Category` = row_number())
+
+#putting data into long form for plot
+
+test_info_long <- test_info_data %>% gather(key = `Likert Category`, value = testinfo, 1:5)
+
+test_info_long$`Likert Category` <- factor(test_info_long$`Likert Category`, ordered = TRUE, levels = c("Three", "Five", "Seven", "Nine", "Eleven"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+plot(eleven_cat_graded, type = "rxx")
+
 extr.11.1 <- extract.item(eleven_cat_graded, 1)
 info.11.1 <- iteminfo(extr.11.1, Theta)
 
@@ -268,6 +359,8 @@ plot(y = test.info.11.data$test.info.11.sum, x = test.info.11.data$V11)
 #built in-mirt plot
 plot(eleven_cat_graded, type = 'info')
 
+#reliability is info - 1 / info
+
 #plotting extracted test info 
 plot(y = test.info.11.data$mirt, x = test.info.11.data$V11)
 
@@ -285,8 +378,13 @@ ICCplot = ggplot()+
   geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.8), color = "darkorchid")+
   geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.9), color = "orchid")+
   geom_line(data = test.info.11.data, aes(x = Theta, y = info.11.10), color = "green")+
-  geom_line(data = test.info.11.data, aes(x = Theta, y = test.info.11.sum), color = "black")+
+#  geom_line(data = test.info.11.data, aes(x = Theta, y = test.info.11.sum), color = "black")+
   xlab('Theta') + ylab('Item Information')
 
 print(ICCplot)
+
+#Plot test information function for 
+
+
+itemplot(eleven_cat_graded, 1, type = "info")
 
